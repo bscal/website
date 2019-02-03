f22929f4aead767cb2786fed9312d7a8a97f27e0
Jon Fus <jonfus123@hotmail.com>
2019-02-02 21:14:13 -0500

    Added contact email
    - Added contact email in footer
    - Added ClipBoardJS to easy copy email
    - Change some css
    - edited skillset list

--- client/js/index.js
+++ client/js/index.js
@@ -1,2 +1,11 @@
-const sideNav = document.querySelector(".sidenav");
-M.Sidenav.init(sideNav, {});
+// const sideNav = document.querySelector(".sidenav");
+// M.Sidenav.init(sideNav, {});
+
+var clipboard = new ClipboardJS(".copy-button");
+clipboard.on("success", function(e) {
+    // Alert Toast
+    var toast = Metro.toast.create;
+    toast("Copied!", null, 5000, "toast");
+
+    e.clearSelection();
+});
